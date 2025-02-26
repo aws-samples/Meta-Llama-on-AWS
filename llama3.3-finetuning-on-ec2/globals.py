36b772804bf7ad72a7cb5137327b36e5578f1f89
Madhur Prashant <129979633+madhurprash@users.noreply.github.com>
2025-02-25 19:20:17 -0500

    Delete llama3.3-finetuning-on-ec2/globals.py

--- llama3.3-finetuning-on-ec2/globals.py
+++
@@ -1,9 +0,0 @@
-# contains global variables used across the repository
-
-# Results directory where all the results are stored
-RESULTS_DIR: str = "results"
-
-# Result files for collecting metrics during the training process
-EC2_SYSTEM_METRICS_FNAME: str = "ec2_metrics.csv"
-EC2_UTILIZATION_METRICS_INTERVAL: int = 5
-TRAINING_STATS_FNAME: str = "training_stats.txt"
